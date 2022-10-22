2b4a61c3f298eea84560b6b4f7bb11543224d6ed
Lnx <46533182+lnxAv@users.noreply.github.com>
2022-10-22 01:14:20 -0400

    xdamp without precision test

--- src/@helpers/x-damp.tsx
+++ src/@helpers/x-damp.tsx
@@ -8,16 +8,16 @@ type XDamp = (
   precision?: number
 ) => number;
 
-function precisionRound(number: number, precision: number) {
+/* function precisionRound(number: number, precision: number) {
   // https://stackoverflow.com/questions/1458633/how-to-deal-with-floating-point-number-precision-in-javascript
   const factor = 10 ** precision;
   const n = precision < 0 ? number : 0.01 / factor + number;
   return Math.round(n * factor) / factor;
-}
+} */
 
-export const xDamp: XDamp = (from, to, lambda, delta, precision = 7) => {
+export const xDamp: XDamp = (from, to, lambda, delta) => {
   if (from === to) return from;
-  const result = precisionRound(MathUtils.damp(from, to, lambda, delta), precision);
+  const result = MathUtils.damp(from, to, lambda, delta);
   return result;
 };
 export default xDamp;
